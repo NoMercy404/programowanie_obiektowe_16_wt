c510aef1a1f0038973273812b6b14f633a1cc1bb
Student <lukaslux@outlook.com>
2023-02-28 18:23:26 +0100

    zadanie 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,9 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
-        // write your code here asdasd asdasd asdsad asdasd
+        Point point = new Point(1,1);
+        Segment segment = new Segment(point, new Point(2,2));
+        System.out.println(segment.getSvg());
     }
 }
---
+++ src/com/company/Segment.java
@@ -0,0 +1,42 @@
+package com.company;
+
+import java.util.Locale;
+
+public class Segment {
+
+    private Point p1;
+    private Point p2;
+//" <line x1="20" y1="20" x2="80" y2="80" style="stroke:black" />"
+//    String test = String.format(Locale.ENGLISH, " %f ", 2);
+
+    public Segment(Point p1, Point p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+    }
+
+    public Point getP1() {
+        return p1;
+    }
+
+//    public void setP1(Point p1) {
+//        this.p1 = p1;
+//    }
+
+    public Point getP2() {
+        return p2;
+    }
+
+//    public void setP2(Point p2) {
+//        this.p2 = p2;
+//    }
+
+    public String getSvg(){
+        String result = String.format(Locale.ENGLISH,"<line x1=\"%.2f\" y1=\"%.2f\" x2=\"%.2f\" y2=\"%.2f\" style=\"stroke:black\" />",p1.x,p1.y,p2.x,p2.y);
+        return result;
+    }
+
+    public double getDistance(){
+        double result = Math.sqrt(Math.pow(p1.x-p2.x, 2))+Math.sqrt(Math.pow(p1.y-p2.y, 2));
+        return result;
+    }
+}
