808a3cd9ff219c2bfe33b3259aa1c4a03df1ccd0
Student <lukaslux@outlook.com>
2023-02-28 18:44:20 +0100

    zadanie 34

--- src/com/company/Segment.java
+++ src/com/company/Segment.java
@@ -3,15 +3,22 @@ package com.company;
 import java.util.Locale;
 
 public class Segment {
-
     private Point p1;
     private Point p2;
 //" <line x1="20" y1="20" x2="80" y2="80" style="stroke:black" />"
 //    String test = String.format(Locale.ENGLISH, " %f ", 2);
 
+    //http://maxima.cesga.es/
+    //solve([y1=a*x1+b, ((x0-x1)^2 + (y0-y1)^2)=r*r],[x1,y1]);
+
     public Segment(Point p1, Point p2) {
         this.p1 = p1;
         this.p2 = p2;
+//        points = new Point[] {
+//                new Point(2,3),
+//                new Point(2,4)
+//        };
+//        points[1] = new Point(2,3);
     }
 
     public Point getP1() {
