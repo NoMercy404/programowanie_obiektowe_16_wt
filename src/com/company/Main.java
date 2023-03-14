189e1aaba284ca41c1b109cf44c970cab7d33984
mpiekarz <monika.piekarz@mail.umcs.pl>
2023-03-14 16:41:28 +0100

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,8 @@ import java.util.Locale;
 public class Main {
     public static final Point test = new Point(2,3);
 
+
+
     public static void main(String[] args) {
         Shape poly = new Polygon(new Vec2[]{new Vec2(120,60), new Vec2(270,280), new Vec2(240,320), new Vec2(110,80)});
    
